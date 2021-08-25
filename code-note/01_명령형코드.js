ca05ed2ab35a3fa3a3fa97b05ec957e330db2acb
andrea <kjkandrea@gmail.com>
2021-08-25 23:01:37 +0900

    Add 명령형 누산기

--- code-note/01_명령형코드.js
+++ code-note/01_명령형코드.js
@@ -1,7 +1,23 @@
-function 명령형코드 () {
-  console.log(
-    1+1
-  )
+/**
+ * 리스트에서 홀수를 length 만큼 뽑아서 제곱한 후 모두 더하라
+ */
+function f(list, length) {
+  let i = 0;
+  let acc = 0;
+  for (const int of list) {
+    if (int % 2) {
+      acc = acc + int * int;
+      if (++i === length) break;
+    }
+  }
+  console.log(acc)
 }
-export default 명령형코드;
+
+function main () {
+  f([1,2,3,4,5], 1)
+  f([1,2,3,4,5], 2)
+  f([1,2,3,4,5], 3)
+}
+
+export default main;
 
