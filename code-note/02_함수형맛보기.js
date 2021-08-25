13d6d3d2b3ed77983ac61577aab1e754ba5fc100
andrea <kjkandrea@gmail.com>
2021-08-26 00:17:49 +0900

    Use curry

--- code-note/02_함수형맛보기.js
+++ code-note/02_함수형맛보기.js
@@ -2,36 +2,39 @@
  * 리스트에서 홀수를 length 만큼 뽑아서 제곱한 후 모두 더하라
  */
 
+const curry = f => (a, ...bs) =>
+  bs.length ? f(a, ...bs) : (...bs) => f(a, ...bs);
+
 // 추상화. filter 기능을 위임
 // 홀수를 뽑아라
-function *filter(f, iter) {
+const filter = curry(function *(f, iter) {
   for (const a of iter) {
     if (f(a)) yield a;
   }
-}
+});
 
 // 추상화. mapping 기능을 위임
 // 제곱하여 맵핑하라
-function *map(f, iter) {
+const map = curry(function *(f, iter) {
   for (const a of iter) {
     yield f(a)
   }
-}
+});
 
 // 추상화 take 기능을 위임
 // length 만큼 take 하라
-function take(length, iter) {
+const take = curry(function take(length, iter) {
   const res = [];
   for (const a of iter) {
     res.push(a);
     if (res.length === length) return res;
   }
   return res;
-}
+});
 
 // 추산화 reduce 기능을 위임
 // 누산하라
-function reduce(f, acc, iter) {
+const reduce = curry(function(f, acc, iter) {
   if (arguments.length === 2) { // 난 이게 뭔지 모르겠어. 나중에 천천히 읽어보자
     iter = acc[Symbol.iterator]();
     acc = iter.next().value;
@@ -40,13 +43,9 @@ function reduce(f, acc, iter) {
     acc = f(acc, a)
   }
   return acc
-}
-
-const curry = f => (a, ...bs) =>
-  bs.length ? f(a, ...bs) : (...bs) => f(a, ...bs);
+});
 
 const add = curry((a, b) => a + b);
-console.log(add(10)(1))
 
 // 함수를 리스트로 사고하는 방식. 어렵다.. ㅠㅠ
 // go(10, a => a + 10)
@@ -60,10 +59,10 @@ const f = (list, length) =>
 
 const f2 = (list, length) => go (
   list,
-  list => filter(a => a % 2, list),
-  list => map(a => a * a, list),
-  list => take(length, list),
-  list => reduce(add, 0, list),
+  list => filter(a => a % 2)(list),
+  list => map(a => a * a)(list),
+  list => take(length)(list),
+  list => reduce(add)(list),
 )
 
 function main () {
