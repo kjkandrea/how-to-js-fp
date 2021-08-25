f85e8fe91388c30790cdbc5c997f409eed068a50
andrea <kjkandrea@gmail.com>
2021-08-26 00:33:21 +0900

    Add my answer file

---
+++ code-note/02_나도풀어볼래.js
@@ -0,0 +1,14 @@
+/**
+ * 리스트에서 홀수를 length 만큼 뽑아서 제곱한 후 모두 더하라
+ */
+function f(list, length) {
+  console.log('나도 풀거야!!')
+}
+
+function main () {
+  f([1,2,3,4,5], 1)
+  f([1,2,3,4,5], 2)
+  f([1,2,3,4,5], 3)
+}
+
+export default main;
--- code-note/index.js
+++ code-note/index.js
@@ -1,11 +1,13 @@
 import 명령형코드 from './01_명령형코드.js'
 import 함수형맛보기 from './02_함수형맛보기.js'
+import 나도풀어볼래 from './02_나도풀어볼래.js'
 
 const entries = [
   명령형코드,
-  함수형맛보기
+  함수형맛보기,
+  나도풀어볼래
 ]
 
 const run = chapter => entries[chapter]()
 
-run(1)
+run(2)
