2e36974bf32b920e02b22040ce2aa6a907d8cdbc
andrea <kjkandrea@gmail.com>
2021-08-26 00:37:27 +0900

    Add my answer code
    복잡도가 더 높겠지?

--- code-note/02_나도풀어볼래.js
+++ code-note/02_나도풀어볼래.js
@@ -2,13 +2,17 @@
  * 리스트에서 홀수를 length 만큼 뽑아서 제곱한 후 모두 더하라
  */
 function f(list, length) {
-  console.log('나도 풀거야!!')
+  return list
+    .filter(int => int % 2)
+    .splice(0, length)
+    .map(int => Math.pow(int, 2))
+    .reduce((acc, cur) => acc + cur)
 }
 
 function main () {
-  f([1,2,3,4,5], 1)
-  f([1,2,3,4,5], 2)
-  f([1,2,3,4,5], 3)
+  console.log(f([1,2,3,4,5], 1))
+  console.log(f([1,2,3,4,5], 2))
+  console.log(f([1,2,3,4,5], 3))
 }
 
 export default main;
