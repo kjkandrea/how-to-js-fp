d8a06b4bc22ec5712e7390d689ea110f8e27c1f0
andrea <kjkandrea@gmail.com>
2021-08-27 19:20:22 +0900

    Add users

---
+++ code-note/04_유저목록.js
@@ -0,0 +1,13 @@
+const users = [
+  { name: 'a', age: 21, family: [
+      { name : 'a1', age: 18 }, { name : 'a2', age: 19 },
+      { name : 'a3', age: 18 }, { name : 'a4', age: 17 },
+  ]},
+  { name: 'b', age: 23, family: [
+      { name : 'b1', age: 18 }, { name : 'b2', age: 20 },
+    ]},
+  { name: 'c', age: 22, family: [
+      { name : 'c1', age: 18 }, { name : 'c2', age: 19 },
+      { name : 'c3', age: 18 }, { name : 'c4', age: 17 },
+    ]},
+]
