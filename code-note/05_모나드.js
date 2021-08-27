c4134659411901e5230d9b27a7012ff3b94b2d6a
andrea <kjkandrea@gmail.com>
2021-08-27 19:55:16 +0900

    Add monad code

--- code-note/05_모나드.js
+++ code-note/05_모나드.js
@@ -5,7 +5,13 @@ const f = a => a * a;
 
 function main() {
   go(
-    [1].map(g).map(f),
+    [1].map(g).map(f), // [] 는 모나드이다.
+    // [] 안에 있는 갯수가 여러개여도 합성을 안전하게한다.
+    console.log
+  )
+
+  go (
+    f(g(1)),
     console.log
   )
 }
