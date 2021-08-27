6c8ad56542fe4fa46ec0ea910d943b179708f4e7
andrea <kjkandrea@gmail.com>
2021-08-27 18:31:42 +0900

    Add flat

--- code-note/03_이차원배열.js
+++ code-note/03_이차원배열.js
@@ -4,3 +4,23 @@ const arr = [
   [6, 7, 8],
   [9, 10]
 ]
+const L = {};
+L.flat = function *(iter) {
+  for (const a of iter) {
+     if (a && a[Symbol.iterator]) {
+       for (const b of a) {
+         yield b;
+       }
+     } else {
+       yield a;
+     }
+  }
+}
+
+function main() {
+  console.log(
+    ...L.flat(arr)
+  )
+}
+
+export default main;
--- code-note/index.js
+++ code-note/index.js
@@ -1,13 +1,15 @@
 import 명령형코드 from './01_명령형코드.js'
 import 함수형맛보기 from './02_함수형맛보기.js'
 import 나도풀어볼래 from './02_나도풀어볼래.js'
+import 이차원배열 from './03_이차원배열.js'
 
 const entries = [
   명령형코드,
   함수형맛보기,
-  나도풀어볼래
+  나도풀어볼래,
+  이차원배열
 ]
 
 const run = chapter => entries[chapter]()
 
-run(1)
+run(3)
