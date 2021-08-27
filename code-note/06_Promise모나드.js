8bfbec93198c57f09b4f65a32fe00daf101d9bcc
andrea <kjkandrea@gmail.com>
2021-08-27 20:14:35 +0900

    Add delay
    이해가 잘 안감

--- code-note/06_Promise모나드.js
+++ code-note/06_Promise모나드.js
@@ -6,6 +6,12 @@ const fg = x => Promise.resolve(x)
   .then(g)
   .then(f)
 
+// TODO. delay : 무엇을 설명하려하는지 잘 모르겠음. 한번 더 보기
+const delay = (time, a) => new Promise(resolve =>
+  setTimeout(() => resolve(a), time))
+
+delay(100, 5).then(console.log)
+
 function main() {
   go(
     fg('{"k": 10}').catch(_ => '미안...').then(console.log), // 10
