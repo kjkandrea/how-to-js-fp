b153e829846ddba3c742f8be6a3c1020bac8b75d
andrea <kjkandrea@gmail.com>
2021-08-27 20:29:55 +0900

    Add promise go

--- code-note/06_PromiseGo.js
+++ code-note/06_PromiseGo.js
@@ -3,10 +3,18 @@ import { reduce } from './funtions.js'
 const go1 = (a, f) => a instanceof Promise ? a.then(f) : f(a);
 const go = (...as) => reduce(go1, as);
 
+const delay = (time, a) => new Promise(resolve =>
+  setTimeout(() => resolve(a), time))
+
 function main() {
+  console.log(
   go(
-    Promise.resolve(1),
+    Promise.resolve(1), // 이 라인에 go 는 즉시 Promise 가 된다.
+    a => a + 1,
+    a => delay(100, a + 10000),
+    a => delay(100, a + 10000),
     console.log
+  ) // Promise { <pending> }
   )
 }
 
